d06ba865aa93561d8da5340fcf12fb2e12007acb
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2020-03-21 14:57:41 -0400

    create basic layout and form for home page to connect to mongodb

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,26 @@ import Layout from '../components/Layout'
 
 const Home = ({ data }) => {
 
+  const handleOnSubmit = () => {
+    console.log('form submit')
+  }
+
   return (
     <div>
-      <Layout data={data}>
-        <h1>Collections</h1>
-      </Layout>
+      <div className='container'>
+        <h1>MongoDB Compass Clone</h1>
+        <form onSubmit={handleOnSubmit} className='form'>
+          <div className='input-block'>
+            <label>Hostname</label>
+            <input tyle='text' placeholder='localhost' />
+          </div>
+          <div className='input-block'>
+            <label>Port</label>
+            <input tyle='text' placeholder='27017' />
+          </div>
+          <button>Connect</button>
+        </form>
+      </div>
       <style jsx>{`
         .container {
           max-width: 120rem;
@@ -15,6 +30,43 @@ const Home = ({ data }) => {
           background-color: #f4f2f2;
           box-shadow: 0 2rem 6rem rgba(0, 0, 0, 0.3);
           min-height: 50rem;
+        }
+        h1 {
+          text-align: center;
+          padding-top: 2rem;
+        }
+        label {
+          display: block;
+          font-size: 2rem;
+          margin: .5rem;
+        }
+        input {
+          padding: 1rem;
+          border-radius: .5rem;
+          border: none;
+          box-shadow: 0 .3rem .5rem rgba(0, 0, 0, 0.3);
+          margin-bottom: 1rem;
+        }
+        button {
+          display: block;
+          margin: 1rem auto;
+          width: 16rem;
+          padding: .5rem;
+          background-color: green;
+          color: white;
+          font-size: 1.5rem;
+          border: 1px solid black;
+          border-radius: .5rem;
+          text-transform: uppercase;
+          box-shadow: 0 .3rem .5rem rgba(0, 0, 0, 0.3);
+        }
+        .form {
+          // background-color: blue;
+          max-width: 60rem;
+          margin: 8rem auto;
+        }
+        .input-block {
+          text-align: center;
         }
       `}</style>
     </div>
